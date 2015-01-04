fe60ff23edd7a240935f1c88776678bffbdaf02c
Justin Self <justinself@outlook.com>
2015-01-03 22:51:36 -0600

    added git lg alias and turned off file status for posh git

--- poshGitHelpers.ps1
+++ poshGitHelpers.ps1
@@ -1,3 +1,6 @@
+#git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
+
+$Global:GitPromptSettings.EnableFileStatus = $false;
 
 function g-s(){
   git status
